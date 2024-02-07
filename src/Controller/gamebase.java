e81560eb9d5de098352940456b0710375209978b
Temayoo <Matt.yo95280@gmail.com>
2024-02-07 13:52:46 +0100

    tout qui marche pour la story 4 sauf pas de rappel fonction si il se trompe

--- src/Controller/gamebase.java
+++ src/Controller/gamebase.java
@@ -2,66 +2,56 @@ package src.Controller;
 
 import src.Model.Carte;
 import src.Model.Joueur;
-
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class gamebase {
 
-    // Variable pour déterminer si le jeu est terminé
     private static boolean estTermine = false;
-
-
     private static Joueur joueur1;
     private static Joueur joueur2;
 
+    public static void setJoueurActuel(Joueur joueurActuel) {
+        gamebase.joueurActuel = joueurActuel;
+    }
+    public static Joueur getJoueurActuel() {
+        return joueurActuel;
+    }
 
     private static Carte carteJeu;
 
 
+
     private static Joueur joueurActuel;
 
-    // Méthode pour démarrer le jeu
     public static void demarrerJeu() {
-        // Appeler la méthode demanderPseudos() de la nouvelle classe scannerPseudo
         String[] pseudos = src.View.scannerPseudo.demanderPseudos();
-
-        // Initialisation des joueurs et de la carte
         joueur1 = new src.Model.Joueur(pseudos[0], 5, 5, "1");
         joueur2 = new src.Model.Joueur(pseudos[1], 6, 5, "2");
-        carteJeu = new Carte(12, 11, joueur1, joueur2);  // Passer les joueurs à la carte
-
-        // Générer la carte
+        carteJeu = new Carte(12, 11, joueur1, joueur2);
         carteJeu.genererCarte();
-
-        // Initialiser le joueur actuel de manière aléatoire
         joueurActuel = (Math.random() < 0.5) ? joueur1 : joueur2;
 
-        // Boucle principale du jeu
         while (!estTermine) {
             carteJeu.afficher();
             tourJoueur(joueurActuel);
-            joueurActuel = (joueurActuel == joueur1) ? joueur2 : joueur1; // Changer de joueur
+            joueurActuel = (joueurActuel == joueur1) ? joueur2 : joueur1;
         }
     }
 
-    // Méthode pour gérer le tour d'un joueur
     private static void tourJoueur(Joueur joueur) {
-        // Un Sout pour savoir qui est entrain de jouer
         System.out.println("Tour de " + joueur.obtenirPseudo());
-
-        // Déplacement
         deplacement(joueur);
-
-        // Pose de la croix
         placerX(joueur);
     }
 
     // Méthode pour gérer le déplacement d'un joueur
-    private static void deplacement(Joueur joueur) {
+    public static void deplacement(Joueur joueur) {
         Scanner scanner = new Scanner(System.in);
         boolean mouvementValide = false;
+
         while (!mouvementValide) {
             try {
                 System.out.println("Choisissez une direction (z pour haut, s pour bas, q pour gauche, d pour droite) : ");
@@ -69,33 +59,32 @@ public class gamebase {
 
                 switch (direction) {
                     case "z":
-                        joueur.deplacerVersLeHaut(carteJeu);
+                        joueur.deplacerVersLeHaut(carteJeu.getJoueurs(), carteJeu);
                         mouvementValide = true;
                         break;
                     case "s":
-                        joueur.deplacerVersLeBas(carteJeu);
+                        joueur.deplacerVersLeBas(carteJeu.getJoueurs(), carteJeu);
                         mouvementValide = true;
                         break;
                     case "q":
-                        joueur.deplacerVersLaGauche(carteJeu);
+                        joueur.deplacerVersLaGauche(carteJeu.getJoueurs(), carteJeu);
                         mouvementValide = true;
                         break;
                     case "d":
-                        joueur.deplacerVersLaDroite(carteJeu);
+                        joueur.deplacerVersLaDroite(carteJeu.getJoueurs(), carteJeu);
                         mouvementValide = true;
                         break;
                     default:
                         System.out.println("Direction invalide !");
                 }
-            } catch (java.util.InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Erreur de saisie. Veuillez entrer une direction valide.");
-                scanner.nextLine();  // enlève la ligne incorrecte
+                scanner.nextLine();
             }
         }
         carteJeu.afficher();
     }
 
-    // Méthode pour gérer la pose de la croix par un joueur
     private static void placerX(Joueur joueur) {
         Scanner scanner = new Scanner(System.in);
         boolean coordonneesValides = false;
@@ -108,7 +97,7 @@ public class gamebase {
 
                 if (x < 0 || x > carteJeu.obtenirTailleX() || y < 0 || y > carteJeu.obtenirTailleY()) {
                     System.out.println("Erreur : Veuillez entrer deux entiers valides.");
-                    scanner.nextLine(); // enlève la ligne incorrecte
+                    scanner.nextLine();
                     placerX(joueur);
                 }
 
@@ -121,13 +110,11 @@ public class gamebase {
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Erreur : Veuillez entrer deux entiers valides.");
-                scanner.nextLine(); // enlève la ligne incorrecte
+                scanner.nextLine();
             } catch (NoSuchElementException e) {
                 System.out.println("Erreur : Entrée non valide. Assurez-vous d'entrer deux entiers.");
-                scanner.nextLine(); // enlève la ligne incorrecte
+                scanner.nextLine();
             }
         }
     }
 }
-
-
